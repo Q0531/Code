20a9459ecb9c155c039c6b8bc71927c8c1f95bdd
Ronaldo <YJ_improve@163.com>
2024-04-23 21:17:28 +0800

    1

--- C/Practice/4.23.c
+++ C/Practice/4.23.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <windows.h>
 
 // //1.计算n的阶乘
 // int main(){
@@ -45,8 +47,42 @@
 // }
 
 
-//流式中心靠拢输出
-int main(){
+// //4.流式中心靠拢输出
+// int main(){
+//     char arr1[] = "Let's go warriors!";
+//     char arr2[] = "##################";
+//     int left = 0,right = strlen(arr2)-1;
+//     while(left <= right){
+//         arr2[left] = arr1[left];
+//         arr2[right] = arr1[right];
+//         system("cls");
+//          printf("%s\n",arr2);
+//         left++;
+//         right--;
+//         Sleep(1000);
+//     }
+//     system("pause");
+//     return 0;
+// }
 
+
+//5.三次输入密码登录
+int main(){
+    char password[12] = {0};
+    int i = 0;
+    for(i = 0;i<3;i++){
+        printf("请输入密码：");
+        scanf("%s",password);
+        if(strcmp(password,"Qyjcomeon") == 0){
+            printf("密码正确，登录成功\n");
+            break;
+        }
+        else
+            printf("密码错误\n");
+    }
+    if(i == 3){
+        printf("密码错误已达三次，强制退出系统\n");
+    }
+    system("pause");
     return 0;
 }
