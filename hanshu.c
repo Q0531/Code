81a1e7d2b27ea6a32206d24e3213421adea34cc9
Ronaldo <YJ_improve@163.com>
2025-05-22 18:59:38 +0800

    1

--- hanshu.c
+++ hanshu.c
@@ -1,3 +1,4 @@
-int add(int a, int b){
+#include "a.h"
+int adds(int a, int b){
     return a + b;
 }
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include"a.h"
+#include "a.h"
 int main(){
-    int a = add(1, 2);
+    int a = adds(1, 2);
     printf("123and%d",a);
     system("pause");
     return 0;
